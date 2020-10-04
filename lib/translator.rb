a89b9db45ceac120d56d1b0c9667bf91be24e81f
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-04 15:27:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_english_meaning(emotes, emoticon)
   load_library(emotes).each do |key, value|
     if value[:japanese] != emoticon
       return value[:english]
-      binding.pry
+    #  binding.pry
     end
   end
 #  if emoticon ==
