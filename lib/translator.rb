3fe682680c44200111d00fc7f70a294b45db8945
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-04 15:14:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,8 @@ def load_library (emotes)
   final = {}
   YAML.load_file(emotes).each do |key, value|
     final[key] = {}
-    final[key][:english] = ""
-    final[key][:japanese] = ""
+    final[key][:english] = value[0]
+    final[key][:japanese] = value[1]
     #binding.pry
   end
   final
