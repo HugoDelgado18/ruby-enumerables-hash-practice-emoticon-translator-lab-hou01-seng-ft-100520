ff24419db9969262112831d601a585fc3921ed98
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-04 15:37:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 
 def get_japanese_emoticon (emotes, emoticon)
   load_library(emotes).each do |key, value|
-    if value[:english] = emoticon
+    if value[:english] == emoticon
       return value[:japanese]
       return "Sorry, that emoticon was not found"
     #  binding.pry
